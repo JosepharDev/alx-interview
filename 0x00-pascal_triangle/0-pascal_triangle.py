579819deccf30520a9bbd88ed1dfaa5008b30313
JosepharDev <josephardev@gmail.com>
2024-10-03 22:36:51 +0100

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return list()
+    first_row = [1]
+    rows = [first_row]
+
+    for _ in range(1, n):
+        row = [1]
+        for i in range(1, len(rows[ -1])):
+            num = rows[-1][i-1] + rows[-1][i]
+            row.append(num)
+        row.append(1)
+        rows.append(row)
+    return rows
