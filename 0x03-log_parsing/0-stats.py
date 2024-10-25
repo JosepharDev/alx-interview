d09b0cb35edeaf1bb453913431c80193e9fc63a5
root <josephardev@gmail.com>
2024-10-25 08:39:14 +0000

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,50 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
 
 import sys
-import signal
 import re
-"""script that reads stdin line by line and computes metrics """
-
-total_size = 0
-line_count = 0
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                '403': 0, '404': 0, '405': 0, '500': 0}
-status_codes_set = set(status_codes.keys())
-
-log_pattern = re.compile(
-    r'^\S+ - \[\S+ \S+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$'
-)
 
 
-def print_statistics():
-    """Print accumulated statistics."""
-    print(f"Total file size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
 
-def signal_handler(sig, frame):
-    """Handle keyboard interruption to print statistics and exit."""
-    print_statistics()
-    sys.exit(0)
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-signal.signal(signal.SIGINT, signal_handler)
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
+                # File size
+                log["file_size"] += file_size
 
-for line in sys.stdin:
-    line = line.strip()
-    match = log_pattern.match(line)
-    if match:
-        status_code, file_size = match.groups()
-        file_size = int(file_size)
-        total_size += file_size
-        if status_code in status_codes_set:
-            status_codes[status_code] += 1
-    line_count += 1
-    if line_count % 10 == 0:
-        print_statistics()
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
