7ffc3207f30561042111306fe201b7991c873e68
JosepharDev <josephardev@gmail.com>
2024-11-15 04:05:05 -0800

    change to promise

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,13 +5,25 @@ request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/', { 
   if (error) {
     console.log(error);
   }
-  const actorurl = body.characters;
-  actorurl.forEach(urls => {
-    request(urls, { json: true }, (err, res, actorbody) => {
-      if (err) {
-        console.log(error);
-      }
-      console.log(actorbody.name);
+  const actorUrls = body.characters;
+  const actorPromises = actorUrls.map(url => {
+    return new Promise((resolve, reject) => {
+      request(url, { json: true }, (err, res, actorBody) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(actorBody.name); // Resolve with actor's name
+        }
+      });
     });
   });
+  Promise.all(actorPromises)
+    .then(actorNames => {
+      actorNames.forEach(name => {
+        console.log(name); // Print each name in order
+      });
+    })
+    .catch(error => {
+      console.error(error); // Print any errors that occur
+    });
 });
